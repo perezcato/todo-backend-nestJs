d4ca572745fafe1f846d22b6f941baaba3cf9bd2
perezcato <perezcatoc@gmail.com>
2022-02-28 04:02:17 +0000

    crud app with cockroachdb, nestJs and typeorm

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: { origin: 'http://localhost:3001' },
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
