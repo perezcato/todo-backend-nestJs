0b1a3a4263036bc1fcaf23f87078f4e5657373c5
perezcato <perezcatoc@gmail.com>
2022-03-03 05:21:01 +0000

    Added authentication for the session

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    // cors: { origin: 'https://todo-next-js-naxi6.ondigitalocean.app' },
-    cors: { origin: 'http://localhost:3001' },
+    cors: { origin: 'https://todo-next-js-naxi6.ondigitalocean.app' },
   });
   await app.listen(process.env.PORT || 3000);
 }
